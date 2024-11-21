f8ed8e567ffe8bbf1617651503517a48b4a7c2ad
smit-shah-GG <smitshahofficial@gmail.com>
2024-11-21 14:05:35 +0530

    integer arguements

--- LaneDetectionWebcamModule.py
+++ LaneDetectionWebcamModule.py
@@ -5,6 +5,7 @@ import utlis
 curveList = []
 avgVal = 10
 
+
 def getLaneCurve(img, display=2):
 
     imgCopy = img.copy()
@@ -20,7 +21,9 @@ def getLaneCurve(img, display=2):
     imgWarpPoints = utlis.drawPoints(imgCopy, points)
 
     #### Step 3
-    middlePoint, imgHist = utlis.getHistogram(imgWarp, display=True, minPer=0.5, region=4)
+    middlePoint, imgHist = utlis.getHistogram(
+        imgWarp, display=True, minPer=0.5, region=4
+    )
     curveAveragePoint, imgHist = utlis.getHistogram(imgWarp, display=True, minPer=0.9)
     curveRaw = curveAveragePoint - middlePoint
 
@@ -34,35 +37,60 @@ def getLaneCurve(img, display=2):
     if display != 0:
         imgInvWarp = utlis.warpImg(imgWarp, points, wT, hT, inv=True)
         imgInvWarp = cv2.cvtColor(imgInvWarp, cv2.COLOR_GRAY2BGR)
-        imgInvWarp[0:hT // 3, 0:wT] = 0, 0, 0
+        imgInvWarp[0 : hT // 3, 0:wT] = 0, 0, 0
         imgLaneColor = np.zeros_like(img)
         imgLaneColor[:] = 0, 255, 0
         imgLaneColor = cv2.bitwise_and(imgInvWarp, imgLaneColor)
         imgResult = cv2.addWeighted(imgResult, 1, imgLaneColor, 1, 0)
         midY = 450
-        cv2.putText(imgResult, str(curve), (wT // 2 - 80, 85), cv2.FONT_HERSHEY_COMPLEX, 2, (255, 0, 255), 3)
-        cv2.line(imgResult, (wT // 2, midY), (wT // 2 + (curve * 3), midY), (255, 0, 255), 5)
-        cv2.line(imgResult, ((wT // 2 + (curve * 3)), midY - 25), (wT // 2 + (curve * 3), midY + 25), (0, 255, 0), 5)
+        cv2.putText(
+            imgResult,
+            str(curve),
+            (wT // 2 - 80, 85),
+            cv2.FONT_HERSHEY_COMPLEX,
+            2,
+            (255, 0, 255),
+            3,
+        )
+        cv2.line(
+            imgResult, (wT // 2, midY), (wT // 2 + (curve * 3), midY), (255, 0, 255), 5
+        )
+        cv2.line(
+            imgResult,
+            ((wT // 2 + (curve * 3)), midY - 25),
+            (wT // 2 + (curve * 3), midY + 25),
+            (0, 255, 0),
+            5,
+        )
         for x in range(-30, 30):
             w = wT // 20
-            cv2.line(imgResult, (w * x + int(curve // 50), midY - 10),
-                     (w * x + int(curve // 50), midY + 10), (0, 0, 255), 2)
+            cv2.line(
+                imgResult,
+                (w * x + int(curve // 50), midY - 10),
+                (w * x + int(curve // 50), midY + 10),
+                (0, 0, 255),
+                2,
+            )
 
     if display == 2:
-        imgStacked = utlis.stackImages(0.7, ([img, imgWarpPoints, imgWarp],
-                                             [imgHist, imgLaneColor, imgResult]))
-        cv2.imshow('ImageStack', imgStacked)
+        imgStacked = utlis.stackImages(
+            0.7, ([img, imgWarpPoints, imgWarp], [imgHist, imgLaneColor, imgResult])
+        )
+        cv2.imshow("ImageStack", imgStacked)
     elif display == 1:
-        cv2.imshow('Result', imgResult)
+        cv2.imshow("Result", imgResult)
 
     #### NORMALIZATION
     curve = curve / 100
-    if curve > 1: curve == 1
-    if curve < -1: curve == -1
+    if curve > 1:
+        curve = 1
+    if curve < -1:
+        curve = -1
 
     return curve
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     cap = cv2.VideoCapture(0)  # Access the webcam
     initialTrackBarVals = [102, 80, 20, 214]
     utlis.initializeTrackbars(initialTrackBarVals)
@@ -77,7 +105,7 @@ if __name__ == '__main__':
         print(curve)
 
         # Press 'q' to exit
-        if cv2.waitKey(1) & 0xFF == ord('q'):
+        if cv2.waitKey(1) & 0xFF == ord("q"):
             break
 
     cap.release()
